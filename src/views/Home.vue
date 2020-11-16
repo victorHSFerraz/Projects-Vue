81fc5c331f46d8e38f80a89152ad6eb9f300de14
Victor Ferraz <victorf178@gmail.com>
2020-11-16 10:31:37 -0300

    SOMA e RESULTADO

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="home">
-    <h1>CALCULADORA</h1>
-    <button class="resultado">123456789</button>
+    <h1></h1>
+    <button class="resultado">{{calculadoraDisplay}}</button>
     <br>
-    <button class="botao0">0</button>
-    <button class="botao1">1</button>
-    <button class="botao2">2</button>
-    <button class="botao3">3</button>
-    <button class="botao4">4</button>
+    {{ Acao }} {{ posicaoAtual }} {{ x }} {{ y }}
     <br>
-    <button class="botao5">5</button>
-    <button class="botao6">6</button>
-    <button class="botao7">7</button>
-    <button class="botao8">8</button>
-    <button class="botao9">9</button>
+    <button class="botao0" @click="alertaValor('0')">0</button>
+    <button class="botao1" @click="alertaValor('1')">1</button>
+    <button class="botao2" @click="alertaValor('2')">2</button>
+    <button class="botao3" @click="alertaValor('3')">3</button>
+    <button class="botao4" @click="alertaValor('4')">4</button>
     <br>
-    <button class="botao+">+</button>
-    <button class="botao-">-</button>
-    <button class="botaoX">X</button>
-    <button class="botao/"> / </button>
-    <button class="botao%">%</button>
+    <button class="botao5" @click="alertaValor('5')">5</button>
+    <button class="botao6" @click="alertaValor('6')">6</button>
+    <button class="botao7" @click="alertaValor('7')">7</button>
+    <button class="botao8" @click="alertaValor('8')">8</button>
+    <button class="botao9" @click="alertaValor('9')">9</button>
     <br>
-    <button class="botaoC">C</button>
-    <button class="botao=">=</button>
-    <button class="botaoSLEEP">SLEEP</button>
+    <button class="botao+" @click="proximaFuncao('soma')">+</button>
+    <button class="botao-" @click="proximaFuncao('menos')">-</button>
+    <button class="botaoX" @click="proximaFuncao('multiplicacao')">X</button>
+    <button class="botao/" @click="proximaFuncao('divisao')"> / </button>
+    <button class="botao%" @click="proximaFuncao('porcentagem')">%</button>
+    <br>
+    <button class="botaoC" @click="proximaFuncao('apagar')">C</button>
+    <button class="botaoPonto" @click="proximaFuncao('ponto')">.</button>
+    <button class="botao=" @click="proximaFuncao('igual')">=</button>
     <br>
     <br>
     
@@ -35,6 +37,10 @@
 
 <style scoped>
 
+h1 {
+  font-size: 40px;
+}
+
 button {
   background-color: rgb(0, 0, 0);
   border: 5px solid rgb(63, 59, 59);
@@ -44,6 +50,7 @@ button {
   font-size: 30px;
   margin: 3px 3px;
   cursor: pointer;
+  width: 100px;
 }
   button:hover{
     background-color: rgb(19, 18, 17);
@@ -64,9 +71,44 @@ button.resultado {
 </style>>
 
 <script>
-
-
 export default {
-  
+  data () {
+    return {
+      calculadoraDisplay : "",
+      x: 0,
+      y: 0,
+      posicaoAtual : 1,
+      Acao: 1
+    }
+  },
+  methods:{
+    alertaValor(valor){
+      this.calculadoraDisplay = this.calculadoraDisplay+valor      
+        if(this.posicaoAtual === 1){
+          this.x = this.calculadoraDisplay
+        }else if(this.posicaoAtual === 2){
+          this.y = this.calculadoraDisplay
+        }
+    },
+    proximaFuncao(valor){
+      if(valor === 'soma'){
+        this.Acao = 1
+        if(this.posicaoAtual === 1){
+          this.x = this.calculadoraDisplay
+          this.posicaoAtual = 2
+        }else{
+          this.y = this.calculadoraDisplay
+          this.posicaoAtual = 3
+        }
+        this.calculadoraDisplay = ""
+      }else if(valor === 'igual') {
+        if(this.Acao === 1) {
+          this.calculadoraDisplay = Number(this.x) + Number(this.y)
+        } 
+      }if(valor === 'apagar') {
+        valor
+      }
+    }
+  }  
 }
 </script>
